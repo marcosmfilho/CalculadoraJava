b7ed790aa0697d5e138315eaf83ff1d3d8f24aec
Bruno Pessoa <Bruno Pessoa>
2016-09-01 17:24:27 -0300

    Vai pegar conflito ae

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -212,13 +212,8 @@ public class Calculadora extends JFrame implements ActionListener {
     	 */
     	double valor = Double.parseDouble( lblTelaVisor.getText() );
       	System.out.println(valor);
-      	System.out.println("Deixa de embassar rapaz!!!!");
-
-        System.out.println("Acabei de mudar aqui oh!!!");
-        System.out.println(valor);
-        System.out.println("RESPEITA TU BICHOOOOOOOOOOOOO");
-        System.out.println("CONFLITO");
-        
+      	
+        System.out.println("AGORA TÁ DIFERENTE!!! DESCULKPA");
     	double resultado = 0;
 
     	if ( buffer != 0 && lastOperation != "" ) {
